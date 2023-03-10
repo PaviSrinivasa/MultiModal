d2a02daaa02069d7e91d68d7a7f26785b0ab4937
PaviSrinivasa <pavithra.srinivasa@mpi.nl>
2023-03-10 14:09:18 +0100

    modified the quotes

--- multimodal/whisper/views.py
+++ multimodal/whisper/views.py
@@ -14,7 +14,7 @@ from subprocess import Popen, PIPE, STDOUT
 @login_required(login_url='login')
 def home(request):
     #expInfo = Info.objects.all().order_by('-id')
-    if request.method == "GET":
+    if request.method == 'GET':
      #   myFilter = ExpFilter(request.GET, queryset=expInfo)
       #  expInfo = myFilter.qs
        # return render(request, 'home.html', {'expInfo': expInfo, 'myFilter': myFilter, })
@@ -25,42 +25,42 @@ def home(request):
 
 
 def create_directory():
-        command = ["qsub test.sge"]
+        command = ['qsub test.sge']
         try:
                 process = Popen(command, stdout=PIPE, stderr=STDOUT)
                 output = process.stdout.read()
                 exitstatus = process.poll()
                 if (exitstatus==0):
-                        return {"status": "Success", "output":str(output)}
+                        return {'status': 'Success', 'output':str(output)}
                 else:
-                        return {"status": "Failed", "output":str(output)}
+                        return {'status': 'Failed', 'output':str(output)}
         except Exception as e:
-                return {"status": "failed", "output":str(e)}
+                return {'status': 'failed', 'output':str(e)}
 
 
 def delete_directory():
-        command = ["bash","multimodal/scripts/file_manipulater.sh","delete"]
+        command = ['bash','multimodal/scripts/file_manipulater.sh','delete']
         try:
                 process = Popen(command, stdout=PIPE, stderr=STDOUT)
                 output = process.stdout.read()
                 exitstatus = process.poll()
                 if (exitstatus==0):
-                        return {"status": "Success", "output":str(output)}
+                        return {'status': 'Success', 'output':str(output)}
                 else:
-                        return {"status": "Failed", "output":str(output)}
+                        return {'status': 'Failed', 'output':str(output)}
         except Exception as e:
-                return {"status": "failed", "output":str(e)}
+                return {'status': 'Failed', 'output':str(e)}
 
 
 @csrf_exempt
 def file_maniputer(request):
         if request.method == 'POST':
                 request_data=json.loads(request.body)
-                if request_data["action"] == "create":
+                if request_data['action'] == 'create':
                         data = create_directory()
-                elif request_data["action"] == "delete":
+                elif request_data['action'] == 'delete':
                         data = delete_directory()
                 else:
-                        data = {"status": "not defined", "output":"not defined"}
+                        data = {'status': 'not defined', 'output':'not defined'}
                 response = HttpResponse(json.dumps(data), content_type='application/json', status=200)
                 return response
